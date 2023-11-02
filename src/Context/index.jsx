661a6414e3088f1ae3fe902c8b671ea312324786
HanielOntiveros <haniel97@hotmail.com>
2023-11-02 10:36:23 -0600

    Search by items input function ready

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,6 +3,7 @@ import { createContext,useState,useEffect } from "react"
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children})=>{
+
     const [count,setCount]=useState(0)
 
     const [isOpenProduct,setIsOpen]=useState(false)
@@ -18,9 +19,24 @@ export const ShoppingCartProvider = ({children})=>{
     const openCheckout = () => { setIsOpenCheckout(true) }
     const closeChekout = () => { setIsOpenCheckout(false) }
 
-
-  
+    const [items,setItems]=useState(null)
+    const [searchByTitle,setSearchByTitle]=useState(null)
+    const [filteredItems,setFilteredItems]=useState(null)
     
+    useEffect (()=> {
+        fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=120')
+        .then(response => response.json())
+        .then(data=> setItems(data))
+    },[])
+
+    const filteredItemByTitle = (items,searchByTitle) => {
+        return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    useEffect (() => {
+        searchByTitle ? setFilteredItems(filteredItemByTitle(items,searchByTitle)):null
+    },[items , searchByTitle])
+
 
     return(
         <ShoppingCartContext.Provider value={{
@@ -38,6 +54,11 @@ export const ShoppingCartProvider = ({children})=>{
             closeChekout,
             order,
             setOrder,
+            items,
+            setItems,
+            searchByTitle,
+            setSearchByTitle,
+            filteredItems,
             
         }}>
             {children}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,52 @@
-import { useState, useEffect } from 'react'
+import { useContext } from 'react'
 import Layout from '../../Components/Layout'
 import Card  from '../../Components/Card'
 import ProductDetail  from '../../Components/ProductDetail'
+import { ShoppingCartContext } from '../../Context'
 
 function Home() {
-  const [items,setItems]=useState(null)
+  const context = useContext(ShoppingCartContext)
 
-  useEffect (()=> {
-      fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=120')
-        .then(response => response.json())
-        .then(data=> setItems(data))
-  },[])
+  const handleChange = (e) => {
+    context.setSearchByTitle(e.target.value)
+  }
+
+  const renderView = () => {
+    if( context.searchByTitle?.length > 0) {
+      if(context.filteredItems?.length > 0 ){
+        return (
+          context.filteredItems?.map(item => (
+            <Card item = {item} key = {item.id} />
+          ))
+        )
+      }else{
+        return(
+          <div>We dont have anything :(</div>
+        )
+      }
+    } else {
+      return(
+        context.items?.map((item) => (
+          <Card item={item} key={item.id}/>
+        ))
+      )
+    }
+  }
+
     return (
       <>
           <Layout>
-            <h1>Home</h1>
+            <div className='flex items-center justify-center relative w-80 mb-4'>
+              <h1 className='font-medium text-xl'>Exclusive Products</h1>
+            </div>
+            <input
+              type='text' 
+              placeholder='Search product' 
+              className='rounded-lg border border-black w-80 p-4 focus:outline-none'
+              onChange={(e) =>handleChange(e)}
+              />
             <div className='py-5 px-10 grid gap-4 grid-cols-4 w-full max-w-screen-lg '> 
-            {
-              items?.map((item) => (
-                <Card item={item} key={item.id}/>
-              ))
-            }
+              { renderView() }
             </div>
             <ProductDetail />
           </Layout>
